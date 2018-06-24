2a318ed941217522b54bcf214eb3dec1d8b3c0c6
Jon White <jon.white2@gmail.com>
2018-06-23 22:02:43 -0400

    test email

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -7,7 +7,7 @@ function sendEmail() {
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
   const msg = {
-    to: 'test@example.com',
+    to: 'jon.white2@gmail.com',
     from: 'test@example.com',
     subject: 'Sending with SendGrid is Fun',
     text: 'and easy to do anywhere, even with Node.js',
