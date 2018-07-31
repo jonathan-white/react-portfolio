d9a758d8f04f815fdc3a967e740ecfa76911806b
Jon White <jon.white2@gmail.com>
2018-07-31 19:46:49 -0400

    updated social icons

--- src/components/Social/Social.js
+++ src/components/Social/Social.js
@@ -13,23 +13,11 @@ const Social = props => (
     			<i className="fab fa-linkedin fa-2x"></i>
     		</a>
     	</li>
-    	<li className="social-icon">
-    		<a href="https://www.facebook.com/jonathan.white.31337" target="_blank" rel="noopener noreferrer">
-    			<i className="fab fa-facebook fa-2x"></i>
-    		</a>
-    	</li>
-        <li className="social-icon">
-            <a href="mailto:jon.white2@gmail.com" target="_blank" rel="noopener noreferrer">
-                <i className="fas fa-envelope fa-2x"></i>
-            </a>
-        </li>
-    	{!true && 
-	    	<li className="social-icon">
-	    		<a href="https://twitter.com/JonsTheName" target="_blank" rel="noopener noreferrer">
-	    			<i className="fab fa-twitter fa-2x"></i>
-	    		</a>
-	    	</li>
-    	}
+      <li className="social-icon">
+        <a href="mailto:jon.white2@gmail.com" target="_blank" rel="noopener noreferrer">
+          <i className="fas fa-envelope fa-2x"></i>
+        </a>
+      </li>
     </ul>
 );
 
