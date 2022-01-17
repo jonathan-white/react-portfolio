7f4262aafd85cca5576b35e55f788ecef8d9eb73
Jonathan White <33507081+jonathan-white@users.noreply.github.com>
2022-01-17 03:06:53 -0500

    updated copyright

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,7 +6,7 @@ const Footer = props => (
 	<footer className="App-footer">
 		<div className="contact-info">
 			<a href="mailto:jon.white2@gmail.com" target="_blank" rel="noopener noreferrer">
-				<span className="copyright">&copy; 2021 Jonathan White</span>
+				<span className="copyright">&copy; 2022 Jonathan White</span>
 			</a>
 		</div>
 		<Social />
